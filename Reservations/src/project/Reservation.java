83b2f981309dcd7ebce622ec028673ac9f7b44c5
fontai1 <fontai1@CPSC-VM-59.uni.adr.unbc.ca>
2017-10-13 14:08:38 -0700

    renamed project

--- Reservations/src/project/Reservation.java
+++ Reservations/src/project/Reservation.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package reservations;
+package project;
 
 /**
  *
  * @author fontai1
  */
-public class User {
+public class Reservation {
     
 }
