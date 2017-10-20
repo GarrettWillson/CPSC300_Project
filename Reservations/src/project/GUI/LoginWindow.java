0a6370ecec484d7f397c0c6ba1ef893fd465cc52
qqqjj <qqqjj@DESKTOP-49G6HHF>
2017-10-20 15:34:22 +0800

    staff login

--- Reservations/src/project/GUI/LoginWindow.java
+++ Reservations/src/project/GUI/LoginWindow.java
@@ -5,15 +5,145 @@
  */
 package project.GUI;
 
-import javax.swing.JFrame;
+import java.awt.EventQueue;
 
-/**
- *
- * @author fontai1
- */
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JSeparator;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
+
 public class LoginWindow {
-    JFrame loginWindow;
-    public static void makeNewWindow() {
-        
-    }
+
+	private JFrame frame;
+	private JPasswordField textPassword;
+	private JTextField textName;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LoginWindow window = new LoginWindow();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public LoginWindow() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setFont(new Font("Segoe UI Black", Font.PLAIN, 14));
+		frame.setBounds(100, 100, 542, 361);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblLog = new JLabel("Login");
+		lblLog.setFont(new Font("Segoe UI Black", Font.PLAIN, 26));
+		lblLog.setBounds(247, 30, 103, 36);
+		frame.getContentPane().add(lblLog);
+		
+		JLabel lblUsername = new JLabel("Name:");
+		lblUsername.setFont(new Font("Segoe UI Black", Font.PLAIN, 17));
+		lblUsername.setBounds(131, 124, 77, 14);
+		frame.getContentPane().add(lblUsername);
+		
+		JLabel lblPassword = new JLabel("Password:");
+		lblPassword.setFont(new Font("Segoe UI Black", Font.PLAIN, 17));
+		lblPassword.setBounds(113, 187, 95, 14);
+		frame.getContentPane().add(lblPassword);
+		
+		textPassword = new JPasswordField();
+		textPassword.setFont(new Font("Segoe UI Black", Font.PLAIN, 14));
+		textPassword.setBounds(246, 184, 147, 20);
+		frame.getContentPane().add(textPassword);
+		
+		textName = new JTextField();
+		textName.setFont(new Font("Segoe UI Black", Font.PLAIN, 14));
+		textName.setBounds(246, 122, 147, 20);
+		frame.getContentPane().add(textName);
+		textName.setColumns(10);
+		
+		JButton btnLogin = new JButton("Login");
+		btnLogin.setFont(new Font("Segoe UI Black", Font.PLAIN, 14));
+		btnLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				String password= textPassword.getText();
+				String username= textName.getText();
+				
+				//this part check the user name and the password default name "boss"
+				// default password "123456"
+				if (password.contains("123456")&&username.contains("boss")) {
+					
+					
+					textPassword.setText(null);
+					textName.setText(null);
+					
+					//staff info = new staff();
+					Staff.main(null);
+					
+					
+					
+				}else {
+					JOptionPane.showMessageDialog(null, "Invalid name or password","Login Error",JOptionPane.ERROR_MESSAGE);
+				}
+				
+			}
+		});
+		btnLogin.setBounds(113, 246, 89, 23);
+		frame.getContentPane().add(btnLogin);
+		
+		JButton btnReset = new JButton("Reset");
+		btnReset.setFont(new Font("Segoe UI Black", Font.PLAIN, 14));
+		btnReset.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				textName.setText(null);
+				textPassword.setText(null);
+			}
+		});
+		btnReset.setBounds(241, 246, 89, 23);
+		frame.getContentPane().add(btnReset);
+		
+		JButton btnExit = new JButton("Exit");
+		btnExit.setFont(new Font("Segoe UI Black", Font.PLAIN, 14));
+		btnExit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frame=new JFrame("EXIT");
+				if(JOptionPane.showConfirmDialog(frame, "Confirm if you want to exit ", "Login", JOptionPane.YES_NO_OPTION)== JOptionPane.YES_NO_OPTION)
+				{
+					System.exit(0);
+				}
+			}
+		});
+		btnExit.setBounds(366, 246, 89, 23);
+		frame.getContentPane().add(btnExit);
+		
+		JSeparator separator = new JSeparator();
+		separator.setBounds(45, 225, 544, 2);
+		frame.getContentPane().add(separator);
+		
+		JSeparator separator_1 = new JSeparator();
+		separator_1.setBounds(45, 77, 544, 2);
+		frame.getContentPane().add(separator_1);
+	}
 }
--- Reservations/src/project/Startup.java
+++ Reservations/src/project/Startup.java
@@ -18,7 +18,7 @@ public class Startup {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        LoginWindow.makeNewWindow();
+       // LoginWindow.makeNewWindow();
     }
     
 }
