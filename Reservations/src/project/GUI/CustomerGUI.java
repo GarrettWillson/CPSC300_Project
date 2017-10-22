01d632785b7761c3947db6e61a48fe016359a60c
willsong <willsong@localhost>
2017-10-22 12:52:23 -0700

    Merge origin/master
    Conflicts:
    	Reservations/src/project/Reservation.java

--- Reservations/src/project/GUI/CustomerGUI.java
+++ Reservations/src/project/GUI/CustomerGUI.java
@@ -1,6 +1,5 @@
 package project.GUI;
 
-
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -21,124 +20,131 @@ import javax.swing.JButton;
 
 public class CustomerGUI {
 
-	private JFrame frmCustomer;
-	private JTextField txtName;
-	private JTextField txtPhoneNum;
-	private JTextField textField;
+    private JFrame frmCustomer;
+    private JTextField txtName;
+    private JTextField txtPhoneNum;
+    private JTextField textField;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					CustomerGUI window = new CustomerGUI();
-					window.frmCustomer.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    CustomerGUI window = new CustomerGUI();
+                    window.frmCustomer.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 
-	/**
-	 * Create the application.
-	 */
-	public CustomerGUI() {
-		initialize();
-	}
+    /**
+     * Create the application.
+     */
+    public CustomerGUI() {
+        initialize();
+    }
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frmCustomer = new JFrame();
-		frmCustomer.setBounds(100, 100, 745, 504);
-		frmCustomer.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frmCustomer.getContentPane().setLayout(null);
-		
-		JLabel lblCustomer = new JLabel("Customer");
-		lblCustomer.setBounds(200, 11, 300, 41);
-		lblCustomer.setFont(new Font("Gill Sans MT Condensed", Font.BOLD, 45));
-		frmCustomer.getContentPane().add(lblCustomer);
-		
-		JSeparator separator = new JSeparator();
-		separator.setBounds(27, 73, 656, 2);
-		frmCustomer.getContentPane().add(separator);
-		
-		txtName = new JTextField();
-		txtName.setBounds(180, 114, 118, 20);
-		frmCustomer.getContentPane().add(txtName);
-		txtName.setColumns(10);
-		
-		JLabel lblUserName = new JLabel("User Name:");
-		lblUserName.setBounds(73, 117, 86, 14);
-		frmCustomer.getContentPane().add(lblUserName);
-		
-		JLabel lblPhoneNumber = new JLabel("Phone number:");
-		lblPhoneNumber.setBounds(55, 160, 93, 14);
-		frmCustomer.getContentPane().add(lblPhoneNumber);
-		
-		txtPhoneNum = new JTextField();
-		txtPhoneNum.setBounds(180, 157, 118, 20);
-		frmCustomer.getContentPane().add(txtPhoneNum);
-		txtPhoneNum.setColumns(10);
-		
+    /**
+     * Initialize the contents of the frame.
+     */
+    private void initialize() {
+        frmCustomer = new JFrame();
+        frmCustomer.setBounds(100, 100, 745, 504);
+        frmCustomer.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frmCustomer.getContentPane().setLayout(null);
+
+        JLabel lblCustomer = new JLabel("Customer");
+        lblCustomer.setBounds(200, 11, 300, 41);
+        lblCustomer.setFont(new Font("Gill Sans MT Condensed", Font.BOLD, 45));
+        frmCustomer.getContentPane().add(lblCustomer);
+
+        JSeparator separator = new JSeparator();
+        separator.setBounds(27, 73, 656, 2);
+        frmCustomer.getContentPane().add(separator);
+
+        txtName = new JTextField();
+        txtName.setBounds(180, 114, 118, 20);
+        frmCustomer.getContentPane().add(txtName);
+        txtName.setColumns(10);
+
+        JLabel lblUserName = new JLabel("User Name:");
+        lblUserName.setBounds(73, 117, 86, 14);
+        frmCustomer.getContentPane().add(lblUserName);
+
+        JLabel lblPhoneNumber = new JLabel("Phone number:");
+        lblPhoneNumber.setBounds(55, 160, 93, 14);
+        frmCustomer.getContentPane().add(lblPhoneNumber);
+
+        txtPhoneNum = new JTextField();
+        txtPhoneNum.setBounds(180, 157, 118, 20);
+        frmCustomer.getContentPane().add(txtPhoneNum);
+        txtPhoneNum.setColumns(10);
+
 		//JDateChooser dateChooser= new JDateChooser();
-		
-		JSeparator separator_1 = new JSeparator();
-		separator_1.setBounds(27, 222, 656, -6);
-		frmCustomer.getContentPane().add(separator_1);
-		
-		JSeparator separator_2 = new JSeparator();
-		separator_2.setBounds(27, 309, 656, 2);
-		frmCustomer.getContentPane().add(separator_2);
-		
-		JComboBox numPeople = new JComboBox();
-		numPeople.setModel(new DefaultComboBoxModel(new String[] {"2 People", "3 People", "4 People", "5 People", "6 People", "7 People", "8 People", "9 People", "10 People", "11 People", "12 People", "13 People", "14 People", "15 People", "16 People", "17 People", "18 People", "19 People", "20 People"}));
-		numPeople.setBounds(180, 203, 118, 20);
-		frmCustomer.getContentPane().add(numPeople);
-		
-		JLabel lblnumPeople = new JLabel("No. of seat:");
-		lblnumPeople.setBounds(73, 206, 64, 14);
-		frmCustomer.getContentPane().add(lblnumPeople);
-		
-		JLabel lblDate = new JLabel("Date:");
-		lblDate.setBounds(356, 117, 46, 14);
-		frmCustomer.getContentPane().add(lblDate);
-		
-		JRadioButton rdbtnDurition3 = new JRadioButton("3 hours");
-		rdbtnDurition3.setBounds(353, 202, 109, 23);
-		frmCustomer.getContentPane().add(rdbtnDurition3);
-		
-		JRadioButton rdbtnHours = new JRadioButton("4 hours");
-		rdbtnHours.setBounds(464, 202, 109, 23);
-		frmCustomer.getContentPane().add(rdbtnHours);
-		
-		JLabel lblTime = new JLabel("Time:");
-		lblTime.setBounds(356, 160, 46, 14);
-		frmCustomer.getContentPane().add(lblTime);
-		
-		textField = new JTextField();
-		textField.setBounds(412, 157, 124, 20);
-		frmCustomer.getContentPane().add(textField);
-		textField.setColumns(10);
-		
-		JToolBar toolBar = new JToolBar();
-		toolBar.setBounds(10, 11, 87, 16);
-		frmCustomer.getContentPane().add(toolBar);
-		
-		JDateChooser dateChooser = new JDateChooser();
-		dateChooser.setBounds(412, 114, 124, 20);
-		frmCustomer.getContentPane().add(dateChooser);
-		
-		JButton btnSubmit = new JButton("Submit");
-		btnSubmit.setBounds(209, 248, 89, 23);
-		frmCustomer.getContentPane().add(btnSubmit);
-		
-		JButton btnCancel = new JButton("Cancel");
-		btnCancel.setBounds(356, 248, 89, 23);
-		frmCustomer.getContentPane().add(btnCancel);
-	}
+        JSeparator separator_1 = new JSeparator();
+        separator_1.setBounds(27, 222, 656, -6);
+        frmCustomer.getContentPane().add(separator_1);
+
+        JSeparator separator_2 = new JSeparator();
+        separator_2.setBounds(27, 309, 656, 2);
+        frmCustomer.getContentPane().add(separator_2);
+
+        JComboBox numPeople = new JComboBox();
+        numPeople.setModel(new DefaultComboBoxModel(new String[]{"2 People", "3 People", "4 People", "5 People", "6 People", "7 People", "8 People", "9 People", "10 People", "11 People", "12 People", "13 People", "14 People", "15 People", "16 People", "17 People", "18 People", "19 People", "20 People"}));
+        numPeople.setBounds(180, 203, 118, 20);
+        frmCustomer.getContentPane().add(numPeople);
+
+        JLabel lblnumPeople = new JLabel("No. of seat:");
+        lblnumPeople.setBounds(73, 206, 64, 14);
+        frmCustomer.getContentPane().add(lblnumPeople);
+
+        JLabel lblDate = new JLabel("Date:");
+        lblDate.setBounds(356, 117, 46, 14);
+        frmCustomer.getContentPane().add(lblDate);
+
+        JRadioButton rdbtnDurition3 = new JRadioButton("3 hours");
+        rdbtnDurition3.setBounds(353, 202, 109, 23);
+        frmCustomer.getContentPane().add(rdbtnDurition3);
+
+        JRadioButton rdbtnHours = new JRadioButton("4 hours");
+        rdbtnHours.setBounds(464, 202, 109, 23);
+        frmCustomer.getContentPane().add(rdbtnHours);
+
+        JLabel lblTime = new JLabel("Time:");
+        lblTime.setBounds(356, 160, 46, 14);
+        frmCustomer.getContentPane().add(lblTime);
+
+        textField = new JTextField();
+        textField.setBounds(412, 157, 124, 20);
+        frmCustomer.getContentPane().add(textField);
+        textField.setColumns(10);
+
+        JToolBar toolBar = new JToolBar();
+        toolBar.setBounds(10, 11, 87, 16);
+        frmCustomer.getContentPane().add(toolBar);
+
+        JDateChooser dateChooser = new JDateChooser();
+        dateChooser.setBounds(412, 114, 124, 20);
+        frmCustomer.getContentPane().add(dateChooser);
+
+        JButton btnSubmit = new JButton("Submit");
+        btnSubmit.setBounds(209, 248, 89, 23);
+        frmCustomer.getContentPane().add(btnSubmit);
+
+        JButton btnCancel = new JButton("Cancel");
+        btnCancel.setBounds(356, 248, 89, 23);
+        frmCustomer.getContentPane().add(btnCancel);
+    }
+
+    public void customerReservationSubmitButton() {
+
+    }
+
+    public void customerReservationCancelButton() {
+    }
+
 }
--- Reservations/src/project/GUI/CutomerLogin.java
+++ Reservations/src/project/GUI/CutomerLogin.java
@@ -13,111 +13,121 @@ import java.awt.event.ActionEvent;
 
 public class CutomerLogin {
 
-	private JFrame frame;
-	private JTextField txtName;
-	private JTextField txtPhoneNum;
+    private JFrame frame;
+    private JTextField txtName;
+    private JTextField txtPhoneNum;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					CutomerLogin window = new CutomerLogin();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    CutomerLogin window = new CutomerLogin();
+                    window.frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 
-	/**
-	 * Create the application.
-	 */
-	public CutomerLogin() {
-		initialize();
-	}
+    /**
+     * Create the application.
+     */
+    public CutomerLogin() {
+        initialize();
+    }
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 469, 324);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
-		
-		JLabel lblCustomerLogin = new JLabel("Customer Login");
-		lblCustomerLogin.setFont(new Font("Times New Roman", Font.BOLD, 22));
-		lblCustomerLogin.setBounds(136, 11, 163, 48);
-		frame.getContentPane().add(lblCustomerLogin);
-		
-		JSeparator separator = new JSeparator();
-		separator.setBounds(58, 57, 331, 2);
-		frame.getContentPane().add(separator);
-		
-		JLabel lblUserName = new JLabel("User Name:");
-		lblUserName.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblUserName.setBounds(58, 95, 86, 14);
-		frame.getContentPane().add(lblUserName);
-		
-		JLabel lblPhoneNum = new JLabel("Phone Num:");
-		lblPhoneNum.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblPhoneNum.setBounds(58, 149, 86, 14);
-		frame.getContentPane().add(lblPhoneNum);
-		
-		txtName = new JTextField();
-		txtName.setBounds(169, 92, 86, 20);
-		frame.getContentPane().add(txtName);
-		txtName.setColumns(10);
-		
-		txtPhoneNum = new JTextField();
-		txtPhoneNum.setBounds(169, 146, 86, 20);
-		frame.getContentPane().add(txtPhoneNum);
-		txtPhoneNum.setColumns(10);
-		
-		JButton btnLogin = new JButton("Login");
-		btnLogin.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		btnLogin.setBounds(300, 91, 89, 23);
-		frame.getContentPane().add(btnLogin);
-		
-		JButton btnCancel = new JButton("Cancel");
-		btnCancel.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				txtName.setText(null);
-				txtPhoneNum.setText(null);
-			}
-		});
-		btnCancel.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		btnCancel.setBounds(300, 145, 89, 23);
-		frame.getContentPane().add(btnCancel);
-		
-		JSeparator separator_1 = new JSeparator();
-		separator_1.setBounds(55, 194, 334, -11);
-		frame.getContentPane().add(separator_1);
-		
-		JSeparator separator_2 = new JSeparator();
-		separator_2.setBounds(65, 194, 324, -11);
-		frame.getContentPane().add(separator_2);
-		
-		JSeparator separator_3 = new JSeparator();
-		separator_3.setBounds(58, 194, 331, 2);
-		frame.getContentPane().add(separator_3);
-		
-		JButton btnAdminLogin = new JButton("Admin login");
-		btnAdminLogin.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		btnAdminLogin.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				LoginWindow.main(null);
-			}
-		});
-		btnAdminLogin.setBounds(65, 207, 116, 23);
-		frame.getContentPane().add(btnAdminLogin);
-		
-		JButton btnCreatNew = new JButton("Creat new");
-		btnCreatNew.setBounds(248, 207, 89, 23);
-		frame.getContentPane().add(btnCreatNew);
-	}
+    /**
+     * Initialize the contents of the frame.
+     */
+    private void initialize() {
+        frame = new JFrame();
+        frame.setBounds(100, 100, 469, 324);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setLayout(null);
+
+        JLabel lblCustomerLogin = new JLabel("Customer Login");
+        lblCustomerLogin.setFont(new Font("Times New Roman", Font.BOLD, 22));
+        lblCustomerLogin.setBounds(136, 11, 163, 48);
+        frame.getContentPane().add(lblCustomerLogin);
+
+        JSeparator separator = new JSeparator();
+        separator.setBounds(58, 57, 331, 2);
+        frame.getContentPane().add(separator);
+
+        JLabel lblUserName = new JLabel("User Name:");
+        lblUserName.setFont(new Font("Tahoma", Font.PLAIN, 14));
+        lblUserName.setBounds(58, 95, 86, 14);
+        frame.getContentPane().add(lblUserName);
+
+        JLabel lblPhoneNum = new JLabel("Phone Num:");
+        lblPhoneNum.setFont(new Font("Tahoma", Font.PLAIN, 14));
+        lblPhoneNum.setBounds(58, 149, 86, 14);
+        frame.getContentPane().add(lblPhoneNum);
+
+        txtName = new JTextField();
+        txtName.setBounds(169, 92, 86, 20);
+        frame.getContentPane().add(txtName);
+        txtName.setColumns(10);
+
+        txtPhoneNum = new JTextField();
+        txtPhoneNum.setBounds(169, 146, 86, 20);
+        frame.getContentPane().add(txtPhoneNum);
+        txtPhoneNum.setColumns(10);
+
+        JButton btnLogin = new JButton("Login");
+        btnLogin.setFont(new Font("Tahoma", Font.PLAIN, 14));
+        btnLogin.setBounds(300, 91, 89, 23);
+        frame.getContentPane().add(btnLogin);
+
+        JButton btnCancel = new JButton("Cancel");
+        btnCancel.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                txtName.setText(null);
+                txtPhoneNum.setText(null);
+            }
+        });
+        btnCancel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+        btnCancel.setBounds(300, 145, 89, 23);
+        frame.getContentPane().add(btnCancel);
+
+        JSeparator separator_1 = new JSeparator();
+        separator_1.setBounds(55, 194, 334, -11);
+        frame.getContentPane().add(separator_1);
+
+        JSeparator separator_2 = new JSeparator();
+        separator_2.setBounds(65, 194, 324, -11);
+        frame.getContentPane().add(separator_2);
+
+        JSeparator separator_3 = new JSeparator();
+        separator_3.setBounds(58, 194, 331, 2);
+        frame.getContentPane().add(separator_3);
+
+        JButton btnAdminLogin = new JButton("Admin login");
+        btnAdminLogin.setFont(new Font("Tahoma", Font.PLAIN, 14));
+        btnAdminLogin.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+                LoginWindow.main(null);
+            }
+        });
+        btnAdminLogin.setBounds(65, 207, 116, 23);
+        frame.getContentPane().add(btnAdminLogin);
+
+        JButton btnCreatNew = new JButton("Creat new");
+        btnCreatNew.setBounds(248, 207, 89, 23);
+        frame.getContentPane().add(btnCreatNew);
+    }
+
+    public void customerLoginLoginButton() {
+
+    }
+
+    public void customerLoginAdminloginButton() {
+    }
+
+    public void customerLoginCreateButton() {
+    }
 }
