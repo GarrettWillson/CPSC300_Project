07355411a93f6d0dd3dab263a96f7b72738105ec
fontai1 <fontai1@localhost>
2017-10-22 12:51:39 -0700

    stuff

---
+++ Reservations/src/project/DataStructures/DataLists.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package project.DataStructures;
+
+import java.util.List;
+import project.Users.Employee;
+
+/**
+ *
+ * @author fontai1
+ */
+public class DataLists {
+    static List<Reservation> reservations;
+    static List<Employee> employees;
+}
