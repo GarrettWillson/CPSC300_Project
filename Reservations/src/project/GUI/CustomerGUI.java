dd1ab79a58259a9dd7515a662302c5cec8909d5c
qiu <qiu@cs-linux-008.unbc.ca>
2017-11-25 12:40:49 -0800

    Customer gui

--- Reservations/src/project/GUI/CustomerGUI.java
+++ Reservations/src/project/GUI/CustomerGUI.java
@@ -297,7 +297,7 @@ public class CustomerGUI {
                 
 		
 		txtpnSpecialRequest = new JTextPane();
-		//txtpnSpecialRequest.setText();
+		txtpnSpecialRequest.setText("SR: ");
 		txtpnSpecialRequest.setBounds(62, 248, 631, 60);
 		frmCustomer.getContentPane().add(txtpnSpecialRequest);
 		//chang user btn
